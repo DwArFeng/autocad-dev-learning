4fa1bf916dc393a34db4b8b39cb7781ff56a4197
DwArFeng <915724865@qq.com>
2019-03-20 15:18:06 +0800

    : 完成 lab_02 代码
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ csharp/Code_Lab_02/LabClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace Code_Lab_02 {
+
+	public class LabClass {
+
+		/// <summary>
+		/// <para>通过控制台提示用户选择一个位置（点），然后将这个位置（点）输出在控制台上。</para>
+		/// <para>在执行命令中，如果用户取消，则输出"发生错误"。</para>
+		/// </summary>
+		[CommandMethod("PromptPoint")]
+		public void PromptPoint() {
+			Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+			Editor editor = doc.Editor;
+
+			PromptPointOptions options = new PromptPointOptions("请选择一个点...");
+			PromptPointResult result = editor.GetPoint(options);
+
+			if (result.Status != PromptStatus.OK) {
+				editor.WriteMessage("发生错误 !");
+				return;
+			}
+
+			editor.WriteMessage("你选择的点是: " + result.Value.ToString());
+		}
+
+	}
+}
