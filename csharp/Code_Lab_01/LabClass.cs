a7d45ad538f06e8f57adf8b63df84127fabf4c4e
DwArFeng <915724865@qq.com>
2019-03-20 15:13:47 +0800

    : 完成 lab_01 代码
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ csharp/Code_Lab_01/LabClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace Code_Lab_01 {
+
+	public class LabClass {
+
+		/// <summary>
+		/// 在CAD的命令窗口栏中输出 "Hello World!" 字符串。
+		/// </summary>
+		[CommandMethod("HelloWorld")]
+		public void HelloWorld() {
+			Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+			editor.WriteMessage("Hello World!");
+		}
+
+	}
+}
